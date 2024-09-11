aa65c8336f2087f8dac1440761f180a822ce3c3f
Chonthicha Maitham <153396723+Chonthicha652115010@users.noreply.github.com>
2024-09-12 01:57:12 +0700

    3.3 set the query follow id which exited and not exited

--- 331-backend/src/main/java/se331/lab/rest/controller/EventController.java
+++ 331-backend/src/main/java/se331/lab/rest/controller/EventController.java
@@ -1,9 +1,12 @@
 package se331.lab.rest.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
 import se331.lab.rest.entity.Event;
 
 import jakarta.annotation.PostConstruct;
@@ -89,19 +92,20 @@ public class EventController {
                 .organizer("Brody Kill")
                 .build());
     }
-    @GetMapping("events")
-    public ResponseEntity<?> getEventLists(@RequestParam(value = "_limit",
-            required = false) Integer perPage, @RequestParam(value="_page", required = false)Integer page){
-        perPage = perPage == null?eventList.size(): perPage;
-        Integer firstIndex = (page-1)*perPage;
-        List<Event> output = new ArrayList<>();
-        try {
-            for (int i = firstIndex; i < firstIndex + perPage; i++) {
-                output.add(eventList.get(i));
+    @GetMapping("events/{id}")
+    public ResponseEntity<?> getEvent(@PathVariable("id")Long id) {
+        Event output = null;
+        for (Event event :
+                eventList) {
+            if (event.getId().equals(id)) {
+                output = event;
+                break;
             }
+        }
+        if (output != null) {
             return ResponseEntity.ok(output);
-        }catch (IndexOutOfBoundsException ex) {
-            return ResponseEntity.ok(output);
+        }else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "the given id is not found");
         }
     }
 }
